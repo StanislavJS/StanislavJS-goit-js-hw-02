9799052041e1a05d2b0943aa5f4ab87e378b2601
StanislavJS <stasyk55@gmail.com>
2025-03-23 18:22:48 +0200

    index.html

--- js/task-3.js
+++ js/task-3.js
@@ -1,16 +1,16 @@
-function getElementWidth(content, padding, border) {
-  // Видаляємо "px" з кожного параметра і перетворюємо на числа
-  const contentWidth = parseFloat(content); // Перетворюємо content на число
-  const paddingWidth = parseFloat(padding); // Перетворюємо padding на число
-  const borderWidth = parseFloat(border); // Перетворюємо border на число
+function checkForSpam(message) {
+  const lowerCaseMessage = message.toLowerCase();
 
-  // Загальна ширина = контент + 2 * (падінг + бордер)
-  const totalWidth = contentWidth + 2 * (paddingWidth + borderWidth);
-
-  return totalWidth;
+  if (lowerCaseMessage.includes('spam') || lowerCaseMessage.includes('sale')) {
+    return true;
+  }
+  return false;
 }
 
-// Перевірка коректності роботи функції
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+console.log(checkForSpam('Latest technology news')); // false
+console.log(checkForSpam('JavaScript weekly newsletter')); // false
+console.log(checkForSpam('Get best sale offers now!')); // true
+console.log(checkForSpam('Amazing SalE, only tonight!')); // true
+console.log(checkForSpam('Trust me, this is not a spam message')); // true
+console.log(checkForSpam('Get rid of sPaM emails. Our book in on sale!')); // true
+console.log(checkForSpam('[SPAM] How to earn fast money?')); // true
